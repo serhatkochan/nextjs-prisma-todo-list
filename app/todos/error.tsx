2f47c68cd5055a19eaa6790d34c3fedfebadc64d
Serhat <serhatkochan@hotmail.com.tr>
2023-07-23 11:33:16 +0300

    created dynamic error page

---
+++ app/todos/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+// "use client" ile client tarafında çalışmasını sağlıyorruz bu componentin.
+// bir hata çıktığında buraya girecek.
+const Error = () => {
+    return (
+        <h1>
+            Error
+        </h1>
+    );
+};
+
+export default Error;
