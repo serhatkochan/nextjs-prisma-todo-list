8e58d1db6351fb885715aea47d1ed807dd4f64b5
Serhat <serhatkochan@hotmail.com.tr>
2023-07-23 11:27:53 +0300

    todos.map

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -3,10 +3,13 @@ import {prisma} from "@/app/db";
 
 const Page = async () => {
     const todos = await prisma.todo.findMany(); // her şeyi döndür
+
     return (
-        <div>
-            Todos Page
-        </div>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>{todo.title}</li>
+            ))}
+        </ul>
     );
 };
 
